6283eb951a92c38b7e6212516b766c17b7ead352
Meng-MiamiOH <43998638+Meng-MiamiOH@users.noreply.github.com>
2023-09-14 11:43:08 -0400

    search function browsing

--- components/index.ts
+++ components/index.ts
@@ -1,10 +1,16 @@
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
 import CustomButton from "./CustomButton";
+import CustomFilter from "./CustomFilter";
+import SearchManugacturer from "./SearchManugacturer";
 
 export { Hero, 
     CustomButton,
     Navbar,
-    Footer
+    Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManugacturer
  };
