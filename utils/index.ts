3e466b80f602de44964e6562be9adbe1d687d843
Meng-MiamiOH <43998638+Meng-MiamiOH@users.noreply.github.com>
2023-09-19 13:58:40 -0400

    url append

--- utils/index.ts
+++ utils/index.ts
@@ -1,11 +1,13 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars(){
+export async function fetchCars(filters: FilterProps){
+    const {manufacturer, year, model, limit, fuel} = filters;
+
     const headers = {
         'X-RapidAPI-Key': '0554c913a5msh439bc1bff832bbep199b6cjsned31e55408ef',
         'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera',{
+    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,{
         headers: headers,
     });
 
